9974a3839cf50108aefb7833fdb90c1bf8e1cf18
murtaza-geeky <bagwalamurtaza@gmail.com>
2017-05-08 00:10:11 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorldApp::Application.config.secret_key_base = '7457a558ad9f09f886a74eaa055e40acaa9e670a16a27bcbda3aa63a1f9456a1d2e2637e6dab8895df4301071b056b1ac347ad68a869eefb01fdb8ada56c8129'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelloWorldApp::Application.config.session_store :cookie_store, key: '_hello_world_app_session'
